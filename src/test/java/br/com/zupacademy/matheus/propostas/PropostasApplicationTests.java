54f3815e8665d39a6666f820a5dd51908e9fe374
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2021-05-10 12:27:56 -0300

    setup projeto

---
+++ src/test/java/br/com/zupacademy/matheus/propostas/PropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.matheus.propostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
